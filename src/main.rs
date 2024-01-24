dc6d4391fe577c7bccdfe20e8411263ece27d599
ohmycloud <ohmycloudy@gmail.com>
2024-01-24 23:06:47 +0800

    init commit

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+//! main.rs
+
+use actix_web::dev::Server;
+use zero2prod::run;
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    // Bubble up the io::Error if we failed to bind the address
+    // Otherwise call .await on our Server
+    run()?.await
+}
