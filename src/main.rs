b7928d5b4940f645a2bcdba2665f317c927fea76
ohmycloud <ohmycloudy@gmail.com>
2024-01-25 22:16:10 +0800

    使用随机端口

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 //! main.rs
 
-use actix_web::dev::Server;
+use std::net::TcpListener;
 use zero2prod::run;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
     // Bubble up the io::Error if we failed to bind the address
     // Otherwise call .await on our Server
-    run()?.await
+    let listener = TcpListener::bind("127.0.0.1:0")
+        .expect("Failed to bind random port");
+    run(listener)?.await
 }
