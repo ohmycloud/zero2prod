6498025d987c65a1f11845edc61a62f554e5bf2e
ohmycloud <ohmycloudy@gmail.com>
2024-03-26 22:39:31 +0800

    PgConnection as dependency injection

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 //! main.rs
 
 use std::net::TcpListener;
+use sqlx::{Connection, PgConnection};
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
 
@@ -12,5 +13,10 @@ async fn main() -> Result<(), std::io::Error> {
     // Otherwise call .await on our Server
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address).expect("Failed to bind random port");
-    run(listener)?.await
+    let configuration = get_configuration().expect("Failed to read configuration");
+    let connection = PgConnection::connect(
+        &configuration.database.connection_string()
+    ).await
+     .expect("Failed to connect to Postgres.");
+    run(listener, connection)?.await
 }
--- src/startup.rs
+++ src/startup.rs
@@ -1,17 +1,27 @@
 use actix_web::dev::Server;
 use std::net::TcpListener;
+use sqlx::PgConnection;
 use actix_web::{web, App, HttpServer};
 use crate::routes::{health_check, subscribe};
 
 // We need to mark `run` as public
 // It is no longer a binary entrypoint, therefore we can mark it as async
 // without having to use any proc-macro incantation.
-pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| {
+pub fn run(
+    listener: TcpListener,
+    connection: PgConnection
+) -> Result<Server, std::io::Error> {
+    // Warp the connection in a smart pointer
+    let connection = web::Data::new(connection);
+    // Capture `connection` from the surrounding environment
+    let server = HttpServer::new(move || {
         App::new()
             .route("/health_check", web::get().to(health_check))
             // A new entry in our routing table for POST /subscriptions requests
             .route("/subscriptions", web::post().to(subscribe))
+            // Register the connection as part of the application state
+            // Get a pointer copy and attach it to the application state
+            .app_data(connection.clone())
     })
         .listen(listener)?
         .run();
