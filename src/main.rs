b40b1e026854ee786e5bfd7a4c98e7d264e77204
ohmycloud <ohmycloudy@gmail.com>
2024-03-26 17:09:23 +0800

    deserialize yaml configuration file to Settings

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
 //! main.rs
 
 use std::net::TcpListener;
+use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
+    // Panic if we can't read configuration
+    let configuration = get_configuration().expect("Failed to read configuration.");
     // Bubble up the io::Error if we failed to bind the address
     // Otherwise call .await on our Server
-    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address).expect("Failed to bind random port");
     run(listener)?.await
 }
