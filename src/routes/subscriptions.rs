00c2a9bffedd2abcc33f2aeaebc928b6ece4f7de
ohmycloud <ohmycloudy@gmail.com>
2024-03-27 17:45:52 +0800

    use span to trace

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -15,12 +15,20 @@ pub async fn subscribe(
     pool: web::Data<PgPool>,
 ) -> HttpResponse {
     let request_id = Uuid::new_v4();
-    tracing::info!(
-        "request_id {} - Adding '{}' '{}' as a new subscriber.",
-        request_id,
-        form.email,
-        form.name
+    // Spans, like logs, have an associated level
+    // `info_span` creates a span at the info-level
+    let request_span = tracing::info_span!(
+        "Adding a new subscriber.",
+        %request_id,
+        subscriber_email = %form.email,
+        subscriber_name = %form.name
     );
+
+    // Using `enter` in an async function is a recipe for disaster!
+    // Bear with me for now, but don't do this at home.
+    // See the following section on `Instrumenting Futures`
+    let _request_span_guard = request_span.enter();
+
     tracing::info!("Saving new subscriber details in the database");
     let query = sqlx::query!(
         r#"
@@ -53,4 +61,6 @@ pub async fn subscribe(
             HttpResponse::InternalServerError().finish()
         }
     }
+    // `_request_span_guard` is dropped at the end of `subscribe`
+    // That's when we "exit" the span
 }
