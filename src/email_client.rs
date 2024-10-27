00ad161806bd6a4a95ab7f2be2a1be6b406ab212
ohmycloud <ohmycloudy@gmail.com>
2024-10-27 18:43:29 +0800

    make test fail

--- src/email_client.rs
+++ src/email_client.rs
@@ -1,7 +1,6 @@
 use crate::domain::SubscriberEmail;
 use reqwest::Client;
 
-
 pub struct EmailClient {
     http_client: Client,
     base_url: String,
@@ -13,7 +12,7 @@ impl EmailClient {
         Self {
             http_client: Client::new(),
             base_url,
-            sender
+            sender,
         }
     }
 
@@ -24,6 +23,41 @@ impl EmailClient {
         html_content: &str,
         text_content: &str,
     ) -> Result<(), String> {
-        todo!()
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use fake::faker::lorem::zh_tw::{Paragraph, Sentence};
+    use wiremock::matchers::any;
+    use wiremock::{Mock, MockServer, ResponseTemplate};
+
+    use crate::domain::SubscriberEmail;
+    use crate::email_client::EmailClient;
+    use fake::faker::internet::en::SafeEmail;
+    use fake::{Fake, Faker};
+
+    #[tokio::test]
+    async fn send_email_fires_a_request_to_base_url() {
+        // Arrange
+        let mock_server = MockServer::start().await;
+        let sender = SubscriberEmail::parse(SafeEmail().fake()).unwrap();
+        let email_client = EmailClient::new(mock_server.uri(), sender);
+
+        Mock::given(any())
+            .respond_with(ResponseTemplate::new(200))
+            .expect(1)
+            .mount(&mock_server)
+            .await;
+        let subscriber_email = SubscriberEmail::parse(SafeEmail().fake()).unwrap();
+        let subject: String = Sentence(1..2).fake();
+        let content: String = Paragraph(1..10).fake();
+
+        // Act
+        let _ = email_client
+            .send_email(subscriber_email, &subject, &content, &content)
+            .await;
+        // Assert
     }
 }
