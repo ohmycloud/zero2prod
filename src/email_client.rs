fc813198a211b4184449681c55b7ebf80b0e6b85
ohmycloud <ohmycloudy@gmail.com>
2025-01-13 15:23:36 +0800

    add error for status

--- src/email_client.rs
+++ src/email_client.rs
@@ -56,14 +56,16 @@ impl EmailClient {
             )
             .json(&request_body)
             .send()
-            .await?;
+            .await?
+            .error_for_status()?;
+
         Ok(())
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use claims::assert_ok;
+    use claims::{assert_err, assert_ok};
     use fake::faker::lorem::zh_tw::{Paragraph, Sentence};
     use secrecy::Secret;
     use wiremock::matchers::{any, header, header_exists, method, path};
@@ -146,5 +148,31 @@ mod tests {
 
         // Assert
         assert_ok!(outcome);
+    }
+
+    #[tokio::test]
+    async fn send_email_fails_if_the_server_returns_500() {
+        // Arrange
+        let mock_server = MockServer::start().await;
+        let sender = SubscriberEmail::parse(SafeEmail().fake()).unwrap();
+        let email_client = EmailClient::new(mock_server.uri(), sender, Secret::new(Faker.fake()));
+
+        let subscriber_email = SubscriberEmail::parse(SafeEmail().fake()).unwrap();
+        let subject: String = Sentence(1..2).fake();
+        let content: String = Paragraph(1..10).fake();
+
+        Mock::given(any())
+            .respond_with(ResponseTemplate::new(500))
+            .expect(1)
+            .mount(&mock_server)
+            .await;
+
+        // Act
+        let outcome = email_client
+            .send_email(subscriber_email, &subject, &content, &content)
+            .await;
+
+        // Assert
+        assert_err!(outcome);
     }
 }
