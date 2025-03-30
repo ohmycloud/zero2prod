eab70306873479e7e19c999aebb98cd6855aa862
ohmycloud <ohmycloudy@gmail.com>
2025-03-30 15:20:10 +0800

    fetch all confirmed subscribers

--- src/routes/newsletters.rs
+++ src/routes/newsletters.rs
@@ -1,5 +1,14 @@
 use actix_web::HttpResponse;
+use actix_web::ResponseError;
+use actix_web::http::StatusCode;
 use actix_web::web;
+use sqlx::PgPool;
+
+use crate::utils::error_chain_fmt;
+
+struct ConfirmedSubscriber {
+    email: String,
+}
 
 #[derive(serde::Deserialize)]
 pub struct Content {
@@ -13,8 +22,50 @@ pub struct BodyData {
     content: Content,
 }
 
+#[derive(thiserror::Error)]
+pub enum PublishError {
+    #[error(transparent)]
+    UnexpectedError(#[from] anyhow::Error),
+}
+
+impl std::fmt::Debug for PublishError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        error_chain_fmt(self, f)
+    }
+}
+
+impl ResponseError for PublishError {
+    fn status_code(&self) -> StatusCode {
+        match self {
+            PublishError::UnexpectedError(_) => StatusCode::INTERNAL_SERVER_ERROR,
+        }
+    }
+}
+
+#[tracing::instrument(name = "Get confirmed subscribers", skip(pool))]
+async fn get_confirmed_subscribers(
+    pool: &PgPool,
+) -> Result<Vec<ConfirmedSubscriber>, anyhow::Error> {
+    let rows = sqlx::query_as!(
+        ConfirmedSubscriber,
+        r#"
+        SELECT email
+        FROM subscriptions
+        WHERE status = 'confirmed'
+        "#,
+    )
+    .fetch_all(pool)
+    .await?;
+
+    Ok(rows)
+}
+
 // We are prefixing `body` with a `_` to avoid
 // a compiler warning about unused arguments
-pub async fn publish_newsletter(_body: web::Json<BodyData>) -> HttpResponse {
-    HttpResponse::Ok().finish()
+pub async fn publish_newsletter(
+    body: web::Json<BodyData>,
+    pool: web::Data<PgPool>,
+) -> Result<HttpResponse, PublishError> {
+    let subscribers = get_confirmed_subscribers(&pool).await?;
+    Ok(HttpResponse::Ok().finish())
 }
