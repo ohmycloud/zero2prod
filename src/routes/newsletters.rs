ec52db85371b9fac91bb21bd62d22db418f02cef
ohmycloud <ohmycloudy@gmail.com>
2025-03-31 16:19:09 +0800

    use a cryptographic hash

--- src/routes/newsletters.rs
+++ src/routes/newsletters.rs
@@ -9,6 +9,7 @@ use anyhow::Context;
 use base64::Engine;
 use secrecy::ExposeSecret;
 use secrecy::Secret;
+use sha3::Digest;
 use sqlx::PgPool;
 
 use crate::domain::SubscriberEmail;
@@ -106,14 +107,17 @@ async fn validate_credentials(
     credentials: Credentials,
     pool: &PgPool,
 ) -> Result<uuid::Uuid, PublishError> {
+    let password_hash = sha3::Sha3_256::digest(credentials.password.expose_secret().as_bytes());
+    // Lowercase hexadecimal encoding
+    let password_hash = format!("{:x}", password_hash);
     let user_id: Option<_> = sqlx::query!(
         r#"
         SELECT user_id
         FROM users
-        WHERE username = $1 AND password = $2
+        WHERE username = $1 AND password_hash = $2
         "#,
         credentials.username,
-        credentials.password.expose_secret()
+        password_hash
     )
     .fetch_optional(pool)
     .await
