df247f7fdf75416979c9cdd28e02b0b030614a19
ohmycloud <ohmycloudy@gmail.com>
2025-04-05 22:34:35 +0800

    return the login HTML page

--- src/routes/login/post.rs
+++ src/routes/login/post.rs
@@ -1,5 +1,5 @@
 use actix_web::http::StatusCode;
-use actix_web::http::header::LOCATION;
+use actix_web::http::header::{ContentType, LOCATION};
 use actix_web::web;
 use actix_web::{HttpResponse, ResponseError};
 use secrecy::Secret;
@@ -26,6 +26,41 @@ impl std::fmt::Debug for LoginError {
 }
 
 impl ResponseError for LoginError {
+    fn error_response(&self) -> HttpResponse {
+        HttpResponse::build(self.status_code())
+            .content_type(ContentType::html())
+            .body(format!(
+                r#"<!DOCTYPE html>
+                <html lang="en">
+                <head>
+                <meta http-equiv="content-type" content="text/html; charset=utf-8">
+                <title>Login</title>
+                </head>
+                <body>
+                <p><i>{}</i></p>
+                <form action="/login" method="post">
+                <label>Username
+                    <input type="text"
+                    placeholder="Enter Username"
+                    name="username"
+                    >
+                </label>
+                <label>Password
+                    <input
+                    type="password"
+                    placeholder="Enter Password"
+                    name="password"
+                    >
+                </label>
+                <button type="submit">Login</button>
+                </form>
+                </body>
+                </html>
+                "#,
+                self
+            ))
+    }
+
     fn status_code(&self) -> StatusCode {
         match self {
             LoginError::AuthError(_) => StatusCode::UNAUTHORIZED,
