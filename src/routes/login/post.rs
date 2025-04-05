b750616b9500c738861e3b5dbad2ebf9f7b17c3c
ohmycloud <ohmycloudy@gmail.com>
2025-04-05 08:56:40 +0800

    a login attempt using random credentials now fail

--- src/routes/login/post.rs
+++ src/routes/login/post.rs
@@ -2,6 +2,9 @@ use actix_web::HttpResponse;
 use actix_web::http::header::LOCATION;
 use actix_web::web;
 use secrecy::Secret;
+use sqlx::PgPool;
+
+use crate::authentication::{Credentials, validate_credentials};
 
 #[derive(serde::Deserialize)]
 pub struct FormData {
@@ -9,8 +12,23 @@ pub struct FormData {
     password: Secret<String>,
 }
 
-pub async fn login(_form: web::Form<FormData>) -> HttpResponse {
-    HttpResponse::SeeOther()
-        .insert_header((LOCATION, "/"))
-        .finish()
+#[tracing::instrument(
+    skip(form, pool),
+    fields(username=tracing::field::Empty, user_id=tracing::field::Empty)
+)]
+pub async fn login(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
+    let credentials = Credentials {
+        username: form.0.username,
+        password: form.0.password,
+    };
+    tracing::Span::current().record("username", &tracing::field::display(&credentials.username));
+    match validate_credentials(credentials, &pool).await {
+        Ok(user_id) => {
+            tracing::Span::current().record("user_id", &tracing::field::display(&user_id));
+            HttpResponse::SeeOther()
+                .insert_header((LOCATION, "/"))
+                .finish()
+        }
+        Err(_) => HttpResponse::Unauthorized().finish(),
+    }
 }
