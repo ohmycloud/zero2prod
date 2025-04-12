d7472e90d9a34c439c1fa98b046fea6d39e9f407
ohmycloud <ohmycloudy@gmail.com>
2025-04-12 17:18:27 +0800

    reformat html

--- src/routes/admin/dashboard.rs
+++ src/routes/admin/dashboard.rs
@@ -40,25 +40,27 @@ pub async fn admin_dashboard(
     Ok(HttpResponse::Ok()
         .content_type(ContentType::html())
         .body(format!(
-            r#"<!DOCTYPE html
+            r#"
+            <!DOCTYPE html>
             <html lang="en">
-            <head>
-              <meta http-equiv="content-type" content="text/html; charset=utf-8">
-              <title>Admin Dashboard</title>
-              </head>
-              <body>
-              <p>Welcome, {username}!</p>
-              <p>Available actions:</p>
-              <ol>
-                  <li><a href="/admin/password">Change password</a></li>
-                  <li>
-                    <form name="logoutForm" action="/admin/logout" method="post">
-                        <input type="submit" value="Logout">
-                    </form>
-                  </li>
-              </ol>
-              </body>
-              </html>
+                <head>
+                    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+                    <title>Admin dashboard</title>
+                </head>
+                <body>
+                    <p>Welcome {username}!</p>
+                    <p>Available actions:</p>
+                    <ol>
+                        <li><a href="/admin/password">Change password</a></li>
+                        <li><a href="/admin/newsletters">Send a newsletter issue</a></li>
+                        <li>
+                            <form name="logoutForm" action="/admin/logout" method="post">
+                                <input type="submit" value="Logout" />
+                            </form>
+                        </li>
+                    </ol>
+                </body>
+            </html>
             "#
         )))
 }
--- src/routes/admin/password/get.rs
+++ src/routes/admin/password/get.rs
@@ -1,4 +1,5 @@
 use crate::session_state::TypedSession;
+use crate::utils::{e500, see_other};
 use actix_web::HttpResponse;
 use actix_web::http::header::ContentType;
 use actix_web_flash_messages::IncomingFlashMessages;
@@ -8,6 +9,10 @@ pub async fn change_pasword_form(
     session: TypedSession,
     flash_messages: IncomingFlashMessages,
 ) -> Result<HttpResponse, actix_web::Error> {
+    if session.get_user_id().map_err(e500)?.is_none() {
+        return Ok(see_other("/login"));
+    }
+
     let mut msg_html = String::new();
     for m in flash_messages.iter() {
         writeln!(msg_html, "<p><i>{}</i></p>", m.content()).unwrap();
@@ -17,46 +22,42 @@ pub async fn change_pasword_form(
         .content_type(ContentType::html())
         .body(format!(
             r#"<!DOCTYPE html>
-                <html lang="en">
+            <html lang="en">
                 <head>
-                    <meta http-equiv="content-type" content="text/html; charset=utf-8">
+                    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
                     <title>Change Password</title>
-                    /head>
-                    <body>
+                </head>
+                <body>
                     {msg_html}
-                    <meta http-equiv="content-type" content="text/html; charset=utf-8">
-                    <title>Change Password</title>
-                    </body>
-                        <form action="/admin/password" method="post">
-                            <label>Current password
+                    <form action="/admin/password" method="post">
+                        <label>Current password
                             <input
-                            type="password"
-                            placeholder="Enter current password"
-                            name="current_password"
+                                type="password"
+                                placeholder="Enter current password"
+                                name="current_password"
                             >
-                            </label>
-                            <br>
-                            <label>New password
+                        </label>
+                        <br>
+                        <label>New password
                             <input
-                            type="password"
-                            placeholder="Enter new password"
-                            name="new_password"
+                                type="password"
+                                placeholder="Enter new password"
+                                name="new_password"
                             >
-                            </label>
-                            <br>
-                            <label>Confirm new password
+                        </label>
+                        <br>
+                        <label>Confirm new password
                             <input
-                            type="password"
-                            placeholder="Type the new password again"
-                            name="new_password_check"
+                                type="password"
+                                placeholder="Type the new password again"
+                                name="new_password_check"
                             >
-                            </label>
-                            <br>
-                            <button type="submit">Change password</button>
-                            </form>
-                            <p><a href="/admin/dashboard">&lt;- Back</a></p>
-                            </body>
-                            </html>
-                "#
+                        </label>
+                        <br>
+                        <button type="submit">Change password</button>
+                    </form>
+                    <p><a href="/admin/dashboard">&lt; - Back</a></p>
+                </body>
+            </html>"#
         )))
 }
