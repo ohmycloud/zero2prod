f44c5c2d2fa394133302ca2d6e6da37e22cf1549
ohmycloud <ohmycloudy@gmail.com>
2025-04-13 14:00:17 +0800

    on conflict do nothing

--- src/idempotency/persistence.rs
+++ src/idempotency/persistence.rs
@@ -5,6 +5,43 @@ use sqlx::PgPool;
 use sqlx::postgres::PgHasArrayType;
 use uuid::Uuid;
 
+pub enum NextAction {
+    StartProcessing,
+    ReturnSavedResponse(HttpResponse),
+}
+
+pub async fn try_processing(
+    pool: &PgPool,
+    idempotency_key: &IdempotencyKey,
+    user_id: Uuid,
+) -> Result<NextAction, anyhow::Error> {
+    let n_inserted_rows = sqlx::query!(
+        r#"
+        INSERT INTO idempotency (
+            user_id,
+            idempotency_key,
+            created_at
+        )
+        VALUES ($1, $2, now())
+        ON CONFLICT DO NOTHING
+        "#,
+        user_id,
+        idempotency_key.as_ref()
+    )
+    .execute(pool)
+    .await?
+    .rows_affected();
+
+    if n_inserted_rows > 0 {
+        Ok(NextAction::StartProcessing)
+    } else {
+        let saved_response = get_saved_response(pool, idempotency_key, user_id)
+            .await?
+            .ok_or_else(|| anyhow::anyhow!("We expected a saved response, we didn't find it"))?;
+        Ok(NextAction::ReturnSavedResponse(saved_response))
+    }
+}
+
 #[derive(Debug, sqlx::Type)]
 #[sqlx(type_name = "header_pair")]
 struct HeaderPairRecord {
@@ -26,9 +63,9 @@ pub async fn get_saved_response(
     let response = sqlx::query!(
         r#"
             SELECT
-                response_status_code,
-                response_headers as "response_headers: Vec<HeaderPairRecord>",
-                response_body
+                response_status_code as "response_status_code!",
+                response_headers as "response_headers!: Vec<HeaderPairRecord>",
+                response_body as "response_body!"
             FROM idempotency
             WHERE
               user_id = $1 AND
--- src/routes/admin/newsletter/post.rs
+++ src/routes/admin/newsletter/post.rs
@@ -1,8 +1,8 @@
 use crate::authentication::UserId;
 use crate::domain::SubscriberEmail;
 use crate::email_client::EmailClient;
-use crate::idempotency::save_response;
-use crate::idempotency::{IdempotencyKey, get_saved_response};
+use crate::idempotency::{IdempotencyKey, NextAction, get_saved_response};
+use crate::idempotency::{save_response, try_processing};
 use crate::utils::{e400, e500, see_other};
 use actix_web::HttpResponse;
 use actix_web::{web, web::ReqData};
@@ -50,6 +50,10 @@ async fn get_confirmed_subscribers(
     Ok(confirmed_subscribers)
 }
 
+fn success_message() -> FlashMessage {
+    FlashMessage::info("The newsletter issue has been published!")
+}
+
 // We are prefixing `body` with a `_` to avoid
 // a compiler warning about unused arguments
 #[tracing::instrument(
@@ -73,13 +77,15 @@ pub async fn publish_newsletter(
     } = form.0;
 
     let idempotency_key: IdempotencyKey = idempotency_key.try_into().map_err(e400)?;
-    // Return early if we have a saved response in the database
-    if let Some(saved_response) = get_saved_response(&pool, &idempotency_key, *user_id)
+    match try_processing(&pool, &idempotency_key, *user_id)
         .await
         .map_err(e500)?
     {
-        FlashMessage::info("The newsletter issue has been published!").send();
-        return Ok(saved_response);
+        NextAction::StartProcessing => {}
+        NextAction::ReturnSavedResponse(saved_response) => {
+            success_message().send();
+            return Ok(saved_response);
+        }
     }
 
     let subscribers = get_confirmed_subscribers(&pool).await.map_err(e500)?;
@@ -111,6 +117,7 @@ pub async fn publish_newsletter(
     }
 
     FlashMessage::info("The newsletter issue has been published!").send();
+    success_message().send();
     let response = see_other("/admin/newsletters");
     let response = save_response(&pool, &idempotency_key, *user_id, response)
         .await
